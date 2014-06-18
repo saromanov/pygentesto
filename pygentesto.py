5195d3089c18ff785e39b3c9740df961b5fe9161
Sergey Romanov <xxsmotur@gmail.com>
2014-06-18 21:45:02 +0600

    Append main file

---
+++ pygentesto.py
@@ -0,0 +1,37 @@
+import gentests
+import tdd
+from genoutput import ConstructUnitTests
+
+import argparse
+
+def inpotp(path):
+	return path[0], path[1]
+
+def runGentests(path):
+	inpfile, outfile = inpotp(path)
+	gen = gentests.GenTests(inpfile)
+	c = ConstructUnitTests(gen.result())
+	c.output(outfile)
+
+def runTDD(path):
+	inpfile, outfile = inpotp(path)
+	t = tdd.TDD()
+	t.parse(inpfile)
+	t.output(outfile)
+
+def parseArguments():
+	parser = argparse.ArgumentParser(description="Some parser")
+	parser.add_argument('-g', dest="gentests", action='store', nargs='+', 
+		help='Parse .py file and create tests')
+	parser.add_argument('--tdd', dest='tdd', nargs='+',
+		help='Create .py files after definition of tests')
+	args = parser.parse_args()
+	if args.tdd != None:
+		runTDD(args.tdd)
+	if args.gentests != None:
+		runGentests(args.gentests)
+
+
+
+if __name__ == '__main__':
+	parseArguments()
